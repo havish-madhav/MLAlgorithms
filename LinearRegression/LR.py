aed35e2b379278c534f50d6163bac2808a32b9fd
havish-madhav <60997271+havish-madhav@users.noreply.github.com>
2020-06-19 13:40:18 +0530

    Create LR.py

---
+++ LinearRegression/LR.py
@@ -0,0 +1,32 @@
+#libraries
+%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+dataset = pd.read_csv('C:/Users/havish/Desktop')
+print(dataset.shape)
+dataset.head()
+# initializing our inputs and outputs
+X = dataset['Head Size(cm^3)'].values
+Y = dataset['Brain Weight(grams)'].values
+# mean of our inputs and outputs
+x_mean = np.mean(X)
+y_mean = np.mean(Y)
+#total number of values
+n = len(X)
+# using the formula to calculate the b1 and b0
+numerator = 0
+denominator = 0
+for i in range(n):
+    numerator += (X[i] - x_mean) * (Y[i] - y_mean)
+    denominator += (X[i] - x_mean) ** 2
+    
+b1 = numerator / denominator
+b0 = y_mean - (b1 * x_mean)
+#printing the coefficient
+print(b1, b0)
+#calculating line values of x and y
+x = np.linspace(x_min, x_max, 1000)
+y = b0 + b1 * x
+#plotting line 
+plt.plot(x, y, color='#00ff00', label='Linear Regression')
